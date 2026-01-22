1a31474311e4024a0927c73aa3940d7a9d1e5d30
ppabst <58234732+ppabst@users.noreply.github.com>
2026-01-22 12:56:50 +0100

    Update uptime_connection_sensor.h
    Remove deprecated uptime connection sensor (ESPHome >= 2025.7)

--- components/cn105/uptime_connection_sensor.h
+++ components/cn105/uptime_connection_sensor.h
@@ -1,18 +1,3 @@
 #pragma once
-#include <esphome/components/uptime/sensor/uptime_seconds_sensor.h>
-namespace esphome {
-    namespace uptime {
-
-        class HpUpTimeConnectionSensor : public UptimeSecondsSensor {
-        public:
-            void update() override;
-            std::string unique_id();
-            void dump_config() override;
-
-            void start();           // connection established
-            void stop();            // connection lost
-        protected:
-            bool connected_{ false };
-        };
-    }  // namespace uptime
-}  // namespace esphome
+// Uptime connection sensor removed.
+// Incompatible with ESPHome >= 2025.7
